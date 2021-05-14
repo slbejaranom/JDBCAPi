e3f57777d36233b78fe3c4217a8eb79decbf087c
SEBASTIAN LEONARDO BEJARANO MEDELLIN <1030671497@bolivar.com>
2021-05-14 10:01:09 -0500

    Cors

--- src/main/java/com/jdbcexample/controller/UsuarioController.java
+++ src/main/java/com/jdbcexample/controller/UsuarioController.java
@@ -3,12 +3,14 @@ package com.jdbcexample.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,6 +18,7 @@ import com.jdbcexample.model.Usuario;
 import com.jdbcexample.service.UsuarioService;
 
 @RestController()
+@CrossOrigin(origins="*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class UsuarioController {
 
 	//LEER todos
